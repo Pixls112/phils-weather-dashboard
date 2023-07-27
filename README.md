563e2faeb10382637273c0b8178c4230fc241915
Pixls112 <nguyen.phillip112@gmai.com>
2023-07-26 23:51:46 -0700

    added comments to script.js, updated style.css, and updated readme

---
+++ README.md
@@ -0,0 +1,40 @@
+# Phils Work Day Scheduler 
+
+## Description
+
+This weeks challenge, I was tasked with creating and application that would use the openweatherapi to display the current weather and show a forecast of the upcoming weather conditions for the next 5 days after today.
+## Installation
+
+To install my code you can use clone the repo using either the HTML or ssh key to add to your local machine.
+
+## Usage
+
+The main usage of this project was to demonstrate my ability to create an application that is able to use the openweatherapi to pull information from it and using javascript allow it to be displayed onto the html so that it will appear in the live deployed web application.
+
+## Screenshot
+
+The following image shows the web application's appearance and functionality:
+
+
+
+## Credit
+
+For this assignment I had collabed with my classmate Alex Rodriguez to finish the assignment.
+
+I would also like to give credit to this youtube video https://www.youtube.com/watch?v=WZNG8UomjSI. It helped point me in the right direction when I was lost and was a useful reference tool when I was confused.
+
+## Access Site
+
+This is the link to access the site launched by github
+
+## Github
+
+https://github.com/Pixls112/phils-weather-dashboard
+
+## Launched Website
+
+
+
+## License
+
+Please refer to the LICENSE in the repo.
--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-
+//This is the main variable that contains all the functions that we can call in console to test if the code works.
 var currentWeather = {
+    //This is used to replace the api key in the api link to easily reference it when using fetch("api link")
     apiKey: "ef5497b9b307f7a9448899e347ab834b",
+    //this function pulls data from the api and can be displayed in console using console.log to test if data is pulled through successfully
     getWeather: function (name) {
         fetch("https://api.openweathermap.org/data/2.5/weather?q="+ name +"&appid="+ this.apiKey +"&units=imperial")
             .then((response) => response.json())
             .then((data) => this.weatherDisplay(data));
     },
-
+    //this function is used to display the current weather information for today in the html for the user to see.
     weatherDisplay: function(data) {
         var i = 0
         var { name } = data
@@ -20,17 +22,18 @@ var currentWeather = {
         document.querySelector(".wind").innerText= speed + " MPH"
         document.querySelector(".weatherIcon").src = "https://openweathermap.org/img/w/"+ icon +".png"
     },
+    //this is used to grab the values of the functions called within it to display when the find function is called upon
     find: function() {
      this.getWeather(document.querySelector(".weatherSearch").value)
      this.getForecast(document.querySelector(".weatherSearch").value)
     },
-
+    //Similar to the getWeather function this is used to see if the data is able to be pulled from the api link. It also allows me to test in console whether the information is displayed 
     getForecast: function(name) {
         fetch("https://api.openweathermap.org/data/2.5/forecast?q="+ name +"&appid=" + this.apiKey +"&units=imperial")
         .then((response) => response.json())
         .then((data) => this.forecastDisplay(data));
     },
-
+    //Similar to the weatherDisplay, this is used to display the upcoming days weather information in the next 5 days
     forecastDisplay: function(data) {
             for (let i = 0; i < data.list.length; i+=8){
                 
@@ -38,10 +41,10 @@ var currentWeather = {
                 var { speed } = data.list[i].wind
                 var { icon } = data.list[i].weather[0]
                 var forecastDate = data.list[i+1].dt
-                var weekDay = new Date(forecastDate*1000)
+                var weekDay = new Date(forecastDate*1000).toDateString()
                 console.log(temp,speed,humidity,icon,forecastDate)
 
-                 var foreCastEl=document.createElement("div")
+                 var foreCastEl=document.createElement("section")
                  foreCastEl.innerHTML=`
                  <h4> ${weekDay} </h4>
                  <p>Temperature: ${temp} </p>
@@ -53,18 +56,18 @@ var currentWeather = {
             }
         }
 }  
-
+//this adds and event listener to the button referenced on the html page so that when the button is clicked it will take the value inputted and apply it into the functions getWeather and GetForecast
 document.querySelector(".submitBtn").addEventListener("click", function(){
   currentWeather.find();
 }) 
 
-
+// this is a simple fuction that displays the current date 
 function displayDate() {
     var currentTime = document.querySelector(".currentDate")
     var rightNow = dayjs().format('dddd, MMM DD, YYYY');
     currentTime.innerText = rightNow;
     
 }
-
+//this calls on the function onto the page to allow it to show on the html
 displayDate()
 
