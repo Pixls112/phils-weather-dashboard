87cc24d214db73642a1c3b8814d2714b599d9649
Pixls112 <nguyen.phillip112@gmai.com>
2023-07-26 22:47:37 -0700

    updated javascript

--- script.js
+++ script.js
@@ -8,15 +8,17 @@ var currentWeather = {
     },
 
     weatherDisplay: function(data) {
+        var i = 0
         var { name } = data
         var { country } = data.sys
         var { temp, humidity} = data.main
         var { speed } = data.wind
-        console.log(name, country)
+        var { icon } = data.weather[i]
         document.querySelector(".currentCity").innerText= name + ", " + country
         document.querySelector(".temp").innerText= temp + " F°"
         document.querySelector(".humidity").innerText= humidity + "%"
         document.querySelector(".wind").innerText= speed + " MPH"
+        document.querySelector(".weatherIcon").src = "https://openweathermap.org/img/w/"+ icon +".png"
     },
     find: function() {
      this.getWeather(document.querySelector(".weatherSearch").value)
@@ -30,19 +32,26 @@ var currentWeather = {
     },
 
     forecastDisplay: function(data) {
-        for (var i = 0; i < 5; i++){
-            foreCast=`<div>
-                <div>
-                    <p>Temp: ` + data.list[i].main.temp + `</p>
-                    <p>Wind: ` + data.list[i].wind.speed + `</p>
-                    <p>Humidity: ` + data.list[i].main.humidity + `</p>
-                </div>
-            </div>
-            `;
+            for (let i = 0; i < data.list.length; i+=8){
+                
+                var { temp, humidity} = data.list[i].main
+                var { speed } = data.list[i].wind
+                var { icon } = data.list[i].weather[0]
+                var forecastDate = data.list[i+1].dt
+                var weekDay = new Date(forecastDate*1000)
+                console.log(temp,speed,humidity,icon,forecastDate)
 
-            $(".upcomingForecast").append(foreCast);
+                 var foreCastEl=document.createElement("div")
+                 foreCastEl.innerHTML=`
+                 <h4> ${weekDay} </h4>
+                 <p>Temperature: ${temp} </p>
+                 <p>Wind: ${speed}</p>
+                 <img src= ${"https://openweathermap.org/img/w/"+ icon +".png"}> 
+                 <p>Humidity: ${humidity}</p>
+                 `
+                 $(".upcomingForecast").append(foreCastEl)
+            }
         }
-}
 }  
 
 document.querySelector(".submitBtn").addEventListener("click", function(){
@@ -54,6 +63,7 @@ function displayDate() {
     var currentTime = document.querySelector(".currentDate")
     var rightNow = dayjs().format('dddd, MMM DD, YYYY');
     currentTime.innerText = rightNow;
+    
 }
 
 displayDate()
